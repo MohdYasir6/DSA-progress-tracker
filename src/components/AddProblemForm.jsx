1ef2015b488b0d92f1c8975851fa79aa6c142dfb
MohdYasir6 <mohdyasir9006@gmail.com>
2026-09-19 01:22:42 -0700

    form css appleid

--- src/components/AddProblemForm.jsx
+++ src/components/AddProblemForm.jsx
@@ -1,3 +1,5 @@
+import "./AddProblemForm.css";
+
 function AddProblemForm({
   name,
   setName,
@@ -12,20 +14,26 @@ function AddProblemForm({
   onSubmit,
 }) {
   return (
-    <form onSubmit={onSubmit}>
+    <form className="add-form" onSubmit={onSubmit}>
       <input
+        className="form-input"
         type="text"
         placeholder="Enter Problem Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <select value={pattern} onChange={(e) => setPattern(e.target.value)}>
+      <select
+        className="form-select"
+        value={pattern}
+        onChange={(e) => setPattern(e.target.value)}
+      >
         <option value="Array">Array</option>
         <option value="Two Pointer">Two Pointer</option>
         <option value="Recursion">Recursion</option>
         <option value="Other">Other</option>
       </select>
       <select
+        className="form-select"
         value={difficulty}
         onChange={(e) => setDifficulty(e.target.value)}
       >
@@ -35,13 +43,17 @@ function AddProblemForm({
         <option value="Hard">Hard</option>
       </select>
       <input
+        className="form-input"
         type="date"
         value={dateSolved}
         onChange={(e) => setDateSolved(e.target.value)}
       />
-      <button type="submit">Add</button>
+      <button className="form-submit-btn" type="submit">
+        Add
+      </button>
       {pattern === "Other" && (
         <input
+          className="form-input"
           type="text"
           placeholder="Enter new pattern name"
           value={customPattern}
